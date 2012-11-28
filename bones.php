f9bf51bf4c60fdd25417c6e4ade4f8a4d4c5535a
rclations <contact@rclations.com>
2012-11-28 10:44:02 -0500

    Removed extra spacing

--- bones.php
+++ bones.php
@@ -256,7 +256,7 @@ function bones_main_nav() {
         'after' => '',                                  // after the menu
         'link_before' => '',                            // before each link
         'link_after' => '',                             // after each link
-        'items_wrap'      => '<ul id="%1$s" class="%2$s">%3$s</ul><a href="#" id="pull">Menu</a>',
+        'items_wrap' => '<ul id="%1$s" class="%2$s">%3$s</ul><a href="#" id="pull">Menu</a>',
         'depth' => 0,                                   // limit the depth of the nav
     	'fallback_cb' => 'bones_main_nav_fallback'      // fallback function
 	));
