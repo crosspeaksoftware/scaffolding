63b6ac9d6c8a3d901c98cb47ddaf89f12b6ce2bb
rclations <contact@rclations.com>
2012-12-16 17:29:22 -0500

    Removed 404 template message & updated default copy

--- 404.php
+++ 404.php
@@ -12,13 +12,13 @@ get_header();
 
 				<header class="article-header">
 
-					<h1><?php _e("Epic 404 - Article Not Found", "bonestheme"); ?></h1>
+					<h1><?php _e("404 Error - Page Not Found", "bonestheme"); ?></h1>
 
 				</header> <!-- end article header -->
 
 				<section class="entry-content">
 
-					<p><?php _e("The article you were looking for was not found, but maybe try looking again!", "bonestheme"); ?></p>
+					<p><?php _e("The page you were looking for was not found, but maybe try looking again!", "bonestheme"); ?></p>
 
 				</section> <!-- end article section -->
 
@@ -27,12 +27,6 @@ get_header();
 					<p><?php get_search_form(); ?></p>
 
 				</section> <!-- end search section -->
-
-				<footer class="article-footer">
-
-					<p><?php _e("This is the 404.php template.", "bonestheme"); ?></p>
-
-				</footer> <!-- end article footer -->
 
 			</article> <!-- end article -->
 
