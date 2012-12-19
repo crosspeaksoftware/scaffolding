crosspeaksoftware/scaffolding

commit 291d1b3ce3fd11c035d1a925c095ae8f86a4d3d6

Changed display of H1 field

functions.php
@@ -139,14 +139,12 @@ add_action( 'admin_menu', 'my_create_post_meta_box' );
 add_action( 'save_post', 'my_save_post_meta_box', 10, 2 );
 
 function my_create_post_meta_box() {
-	add_meta_box( 'my-meta-box', 'Tuck Away H1', 'my_post_meta_box', 'page', 'normal', 'high' );
-	add_meta_box( 'my-meta-box', 'Tuck Away H1', 'my_post_meta_box', 'post', 'normal', 'high' );
+	add_meta_box( 'my-meta-box', 'Tuckaway H1', 'my_post_meta_box', 'page', 'normal', 'high' );
+	add_meta_box( 'my-meta-box', 'Tuckaway H1', 'my_post_meta_box', 'post', 'normal', 'high' );
 }
 
 function my_post_meta_box( $object, $box ) { ?>
 	<p>
-		<label for="second-excerpt">Tuck Away &lt;h1&gt; (appears in the top left for SEO)</label>
-		<br />
 		<input type="text" name="tuck-h1" id="tuck-h1" tabindex="30" class="large-text" value="<?php echo wp_specialchars( get_post_meta( $object->ID, 'Tuck Away H1', true ), 1 ); ?>"/>
 		<input type="hidden" name="my_meta_box_nonce" value="<?php echo wp_create_nonce( plugin_basename( __FILE__ ) ); ?>" />
 	</p>
@@ -160,17 +158,17 @@ function my_save_post_meta_box( $post_id, $post ) {
 	if ( !current_user_can( 'edit_post', $post_id ) )
 		return $post_id;
 
-	$meta_value = get_post_meta( $post_id, 'Tuck Away H1', true );
+	$meta_value = get_post_meta( $post_id, 'Tuckaway H1', true );
 	$new_meta_value = stripslashes( $_POST['tuck-h1'] );
 
 	if ( $new_meta_value && '' == $meta_value )
-		add_post_meta( $post_id, 'Tuck Away H1', $new_meta_value, true );
+		add_post_meta( $post_id, 'Tuckaway H1', $new_meta_value, true );
 
 	elseif ( $new_meta_value != $meta_value )
-		update_post_meta( $post_id, 'Tuck Away H1', $new_meta_value );
+		update_post_meta( $post_id, 'Tuckaway H1', $new_meta_value );
 
 	elseif ( '' == $new_meta_value && $meta_value )
-		delete_post_meta( $post_id, 'Tuck Away H1', $meta_value );
+		delete_post_meta( $post_id, 'Tuckaway H1', $meta_value );
 }
 /************* ADD FIRST AND LAST CLASSES TO MENU & SIDEBAR *****************/
 function add_first_and_last($output) {

commit 04e5a489e010bb11d8c31cc4ad3f5cf8736e4afb

Change get key for tuckaway H1 Fixes #11 which was broken by 291d1b3 when changing name

functions.php
@@ -145,7 +145,7 @@ function my_create_post_meta_box() {
 
 function my_post_meta_box( $object, $box ) { ?>
 	<p>
-		<input type="text" name="tuck-h1" id="tuck-h1" tabindex="30" class="large-text" value="<?php echo wp_specialchars( get_post_meta( $object->ID, 'Tuck Away H1', true ), 1 ); ?>"/>
+		<input type="text" name="tuck-h1" id="tuck-h1" tabindex="30" class="large-text" value="<?php echo wp_specialchars( get_post_meta( $object->ID, 'Tuckaway H1', true ), 1 ); ?>"/>
 		<input type="hidden" name="my_meta_box_nonce" value="<?php echo wp_create_nonce( plugin_basename( __FILE__ ) ); ?>" />
 	</p>
 <?php }