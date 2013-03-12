2fa920fbce7576fe74e9102470c3146ce0536209
rclations <contact@rclations.com>
2013-03-12 09:28:07 -0400

    Merged in https://github.com/eddiemachado/bones/commit/87a7a4eb0a988a166fb52ed0a3862257626dc4b7

--- single.php
+++ single.php
@@ -26,7 +26,7 @@ get_header();
 
 							<h1 class="entry-title single-title" itemprop="headline"><?php the_title(); ?></h1>
 
-							<p class="byline vcard"><?php _e("Posted", "bonestheme"); ?> <time class="updated" datetime="<?php echo the_time('Y-m-j'); ?>" pubdate><?php the_time('F jS, Y'); ?></time> <?php _e("by", "bonestheme"); ?> <span class="author"><?php the_author_posts_link(); ?></span> <span class="amp">&amp;</span> <?php _e("filed under", "bonestheme"); ?> <?php the_category(', '); ?>.</p>
+							<p class="byline vcard"><?php printf(__('Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span> <span class="amp">&amp;</span> filed under %4$s.', 'bonestheme'), get_the_time('Y-m-j'), get_the_time(get_option('date_format')), bones_get_the_author_posts_link(), get_the_category_list(', ')); ?></p>
 
 						</header> <!-- end article header -->
 
