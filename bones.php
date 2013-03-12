1cc212bb91f5b52f1afec29ef2ca511b604e24c9
rclations <contact@rclations.com>
2013-03-12 09:47:25 -0400

    Merged in https://github.com/eddiemachado/bones/commit/f348b1371db57a15f6dd4231e84d9811e73c4205

--- bones.php
+++ bones.php
@@ -423,7 +423,7 @@ function bones_filter_ptags_on_images($content){
 function bones_excerpt_more($more) {
 	global $post;
 	// edit here if you like
-	return '...  <a href="'. get_permalink($post->ID) . '" title="'. __('Read', 'bonestheme') . get_the_title($post->ID).'">'. __('Read more &raquo;', 'bonestheme') .'</a>'';
+	return '...  <a href="'. get_permalink($post->ID) . '" title="'. __('Read', 'bonestheme') . get_the_title($post->ID).'">'. __('Read more &raquo;', 'bonestheme') .'</a>';
 }
 
 /*
