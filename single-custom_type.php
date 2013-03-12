27dfbf63b5f059ab4c20123b9e5914ffd47d1d54
rclations <contact@rclations.com>
2013-03-12 09:46:33 -0400

    Merged in https://github.com/eddiemachado/bones/commit/a82839bf842313aed76bb30a0a02196f212965ba

--- single-custom_type.php
+++ single-custom_type.php
@@ -36,7 +36,7 @@ get_header();
 
 							<?php
 							/* Hidden by default
-							<p class="byline vcard"><?php _e("Posted", "bonestheme"); ?> <time class="updated" datetime="<?php echo the_time('Y-m-j'); ?>" pubdate><?php the_time('F jS, Y'); ?></time> <?php _e("by", "bonestheme"); ?> <span class="author"><?php the_author_posts_link(); ?></span> <span class="amp">&</span> <?php _e("filed under", "bonestheme"); ?> <?php echo get_the_term_list( get_the_ID(), 'custom_cat', "" ) ?>.</p>
+							printf(__('Posted <time class="updated" datetime="%1$s" pubdate>%2$s</time> by <span class="author">%3$s</span> <span class="amp">&</span> filed under %4$s.', 'bonestheme'), get_the_time('Y-m-j'), get_the_time(__('F js, Y', 'bonestheme')), bones_get_the_author_posts_link(), get_the_term_list( $post->ID, 'custom_cat', ' ', ', ', '' ));
 							*/ ?>
 
 						</header> <!-- end article header -->
