196cb1783cb37df7eb70819ed532357474f31e42
rclations <contact@rclations.com>
2013-05-02 13:15:02 -0400

    Updated desktop shortcut icon paths to reflect updated theme structure

--- header.php
+++ header.php
@@ -16,14 +16,14 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1.0"/>
 
 	<!-- icons & favicons (for more: http://www.jonathantneal.com/blog/understand-the-favicon/) -->
-	<link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/library/images/apple-icon-touch.png">
+	<link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/images/apple-icon-touch.png">
 	<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.png">
 	<!--[if IE]>
 	<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
 	<![endif]-->
 	<!-- or, set /favicon.ico for IE10 win -->
 	<meta name="msapplication-TileColor" content="#f01d4f">
-	<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/library/images/win8-tile-icon.png">
+	<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/images/win8-tile-icon.png">
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 
 	<!-- wordpress head functions -->
