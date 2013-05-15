c51ea8e346156a0a3c485af074a6393b61e25c9d
rclations <contact@rclations.com>
2013-05-15 11:17:45 -0400

    Fixed favicon reference

--- header.php
+++ header.php
@@ -17,7 +17,7 @@
 
 	<!-- icons & favicons (for more: http://www.jonathantneal.com/blog/understand-the-favicon/) -->
 	<link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/images/apple-icon-touch.png">
-	<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.png">
+	<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
 	<!--[if IE]>
 	<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
 	<![endif]-->
