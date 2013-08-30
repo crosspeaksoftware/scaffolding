46612fdd7f959d9df610aadaecf2a23fa0da33b8
Tim Howe <timbhowe@gmail.com>
2013-08-30 13:25:53 -0400

    Updated page navigation functions to add the rel nxt/prev http://googlewebmastercentral.blogspot.com/2012/03/video-about-pagination-with-relnext-and.html
    Updated the sticky footer code to work properly and overwrite it in IE7 cause that browser suck wang

--- functions.php
+++ functions.php
@@ -414,61 +414,77 @@ PAGE NAVI
 
 // Numeric Page Navi (built into the theme by default)
 function scaffolding_page_navi($before = '', $after = '') {
-	global $wpdb, $wp_query;
-	$request = $wp_query->request;
-	$posts_per_page = intval(get_query_var('posts_per_page'));
-	$paged = intval(get_query_var('paged'));
-	$numposts = $wp_query->found_posts;
-	$max_page = $wp_query->max_num_pages;
-	if ( $numposts <= $posts_per_page ) { return; }
-	if(empty($paged) || $paged == 0) {
-		$paged = 1;
-	}
-	$pages_to_show = 7;
-	$pages_to_show_minus_1 = $pages_to_show-1;
-	$half_page_start = floor($pages_to_show_minus_1/2);
-	$half_page_end = ceil($pages_to_show_minus_1/2);
-	$start_page = $paged - $half_page_start;
-	if($start_page <= 0) {
-		$start_page = 1;
-	}
-	$end_page = $paged + $half_page_end;
-	if(($end_page - $start_page) != $pages_to_show_minus_1) {
-		$end_page = $start_page + $pages_to_show_minus_1;
-	}
-	if($end_page > $max_page) {
-		$start_page = $max_page - $pages_to_show_minus_1;
-		$end_page = $max_page;
-	}
-	if($start_page <= 0) {
-		$start_page = 1;
-	}
-	echo $before.'<nav class="page-navigation"><ol class="scaffolding_page_navi clearfix">'."";
-	if ($start_page >= 2 && $pages_to_show < $max_page) {
-		$first_page_text = __( "First", 'scaffoldingtheme' );
-		echo '<li class="bpn-first-page-link"><a href="'.get_pagenum_link().'" title="'.$first_page_text.'">'.$first_page_text.'</a></li>';
-	}
-	echo '<li class="bpn-prev-link">';
-	previous_posts_link('<<');
-	echo '</li>';
-	for($i = $start_page; $i  <= $end_page; $i++) {
-		if($i == $paged) {
-			echo '<li class="bpn-current">'.$i.'</li>';
-		}
-		else {
-			echo '<li><a href="'.get_pagenum_link($i).'">'.$i.'</a></li>';
-		}
-	}
-	echo '<li class="bpn-next-link">';
-	next_posts_link('>>');
-	echo '</li>';
-	if ($end_page < $max_page) {
-		$last_page_text = __( "Last", 'scaffoldingtheme' );
-		echo '<li class="bpn-last-page-link"><a href="'.get_pagenum_link($max_page).'" title="'.$last_page_text.'">'.$last_page_text.'</a></li>';
-	}
-	echo '</ol></nav>'.$after."";
+    global $wpdb, $wp_query;
+    $request = $wp_query->request;
+    $posts_per_page = intval(get_query_var('posts_per_page'));
+    $paged = intval(get_query_var('paged'));
+    $numposts = $wp_query->found_posts;
+    $max_page = $wp_query->max_num_pages;
+    if ( $numposts <= $posts_per_page ) { return; }
+    if(empty($paged) || $paged == 0) {
+        $paged = 1;
+    }
+    $pages_to_show = 7;
+    $pages_to_show_minus_1 = $pages_to_show-1;
+    $half_page_start = floor($pages_to_show_minus_1/2);
+    $half_page_end = ceil($pages_to_show_minus_1/2);
+    $start_page = $paged - $half_page_start;
+    if($start_page <= 0) {
+        $start_page = 1;
+    }
+    $end_page = $paged + $half_page_end;
+    if(($end_page - $start_page) != $pages_to_show_minus_1) {
+        $end_page = $start_page + $pages_to_show_minus_1;
+    }
+    if($end_page > $max_page) {
+        $start_page = $max_page - $pages_to_show_minus_1;
+        $end_page = $max_page;
+    }
+    if($start_page <= 0) {
+        $start_page = 1;
+    }
+    echo $before.'<nav class="page-navigation"><ol class="scaffolding_page_navi clearfix">'."";
+    if ($start_page >= 2 && $pages_to_show < $max_page) {
+        $first_page_text = __( "First", 'scaffoldingtheme' );
+        echo '<li class="bpn-first-page-link"><a rel="prev" href="'.get_pagenum_link().'" title="'.$first_page_text.'">'.$first_page_text.'</a></li>';
+    }
+    echo '<li class="bpn-prev-link">';
+    previous_posts_link('<<');
+    echo '</li>';
+    for($i = $start_page; $i  <= $end_page; $i++) {
+        if( $i == $paged ) {
+            echo '<li class="bpn-current">'.$i.'</li>';
+        }elseif( $i == ($paged - 1) ) {
+            echo '<li><a rel="prev" href="'.get_pagenum_link($i).'" title="View Page '.$i.'">'.$i.'</a></li>';
+        }elseif( $i == ($paged + 1) ) {
+            echo '<li><a rel="next" href="'.get_pagenum_link($i).'" title="View Page '.$i.'">'.$i.'</a></li>';
+        }else {
+            echo '<li><a href="'.get_pagenum_link($i).'" title="View Page '.$i.'">'.$i.'</a></li>';
+        }
+    }
+    echo '<li class="bpn-next-link">';
+    next_posts_link('>>');
+    echo '</li>';
+    if ($end_page < $max_page) {
+        $last_page_text = __( "Last", 'scaffoldingtheme' );
+        echo '<li class="bpn-last-page-link"><a rel="next" href="'.get_pagenum_link($max_page).'" title="'.$last_page_text.'">'.$last_page_text.'</a></li>';
+    }
+    echo '</ol></nav>'.$after."";
 } /* end page navi */
 
+//add rel and title attribute to next pagination link
+function get_next_posts_link_attributes($attr){
+    $attr = 'rel="next" title="View the Next Page"';
+    return $attr;
+}
+add_filter('next_posts_link_attributes', 'get_next_posts_link_attributes');
+
+//add rel and title attribute to prev pagination link
+function get_previous_posts_link_attributes($attr){
+    $attr = 'rel="prev" title="View the Previous Page"';
+    return $attr;
+}
+add_filter('previous_posts_link_attributes', 'get_previous_posts_link_attributes');
 
 /*******************
  COMMENT LAYOUT
