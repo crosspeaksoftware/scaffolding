5be030b2347379938ea48ab480e7450b65e3500d
rclations <contact@rclations.com>
2013-10-04 18:46:40 -0400

    Created new file for stock scaffolding functions

---
+++ includes/base-functions.php
@@ -0,0 +1,9 @@
+<?php
+/*
+Author: Hall Internet Marketing
+URL: https://github.com/hallme/scaffolding
+
+All stock functions used on every scaffolding site live here.
+Custom functions go in functions.php to facilitate future updates if necessary.
+*/
+
