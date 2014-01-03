a709639ce6bb023f60839115c547cf3d8e0c3594
Jess <jessicamlavoie@gmail.com>
2014-01-03 10:54:28 -0500

    Added function for captions
    Removes inline width/height attributes for wp-caption.

--- includes/base-functions.php
+++ includes/base-functions.php
@@ -350,3 +350,27 @@ function gform_tabindexer() {
     return GFCommon::$tab_index >= $starting_index ? GFCommon::$tab_index : $starting_index;
 }
 add_filter("gform_tabindex", "gform_tabindexer");
+
+// Filter out hard-coded width, height attributes on all captions (wp-caption class)
+add_shortcode('wp_caption', 'fixed_img_caption_shortcode');
+add_shortcode('caption', 'fixed_img_caption_shortcode');
+function fixed_img_caption_shortcode($attr, $content = null) {
+	if ( ! isset( $attr['caption'] ) ) {
+		if ( preg_match( '#((?:<a [^>]+>\s*)?<img [^>]+>(?:\s*</a>)?)(.*)#is', $content, $matches ) ) {
+			$content = $matches[1];
+			$attr['caption'] = trim( $matches[2] );
+		}
+	}
+	$output = apply_filters('img_caption_shortcode', '', $attr, $content);
+	if ( $output != '' ) return $output;
+	extract(shortcode_atts(array(
+		'id' => '',
+		'align' => 'alignnone',
+		'width' => '',
+		'caption' => ''
+	), $attr));
+	if ( 1 > (int) $width || empty($caption) ) return $content;
+	if ( $id ) $id = 'id="' . esc_attr($id) . '" ';
+	return '<div ' . $id . 'class="wp-caption ' . esc_attr($align) . '" >'
+	. do_shortcode( $content ) . '<p class="wp-caption-text">' . $caption . '</p></div>';
+}
