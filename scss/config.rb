90682816c5dc63a4e515c2b9607e70545dd68f6c
rclations <rc@hallme.com>
2014-01-26 17:20:15 -0500

    Updated the config.rb file

--- scss/config.rb
+++ scss/config.rb
@@ -1,10 +1,7 @@
 # Compass is a great cross-platform tool for compiling SASS. 
-# This compass config file will allow you to 
-# quickly dive right in.
+# This compass config file will allow you to quickly dive right in.
 # For more info about compass + SASS: http://net.tutsplus.com/tutorials/html-css-techniques/using-compass-and-sass-for-css-in-your-next-project/
 
-
-#########
 # 1. Set this to the root of your project when deployed:
 http_path = "/"
 
@@ -13,18 +10,17 @@ css_dir = "../css"
 sass_dir = "./"
 images_dir = "../images"
 javascripts_dir = "../js"
-environment = :development
 relative_assets = true
 
+# 3. Settings for development - enable if desired
+# environment = :development
+# output_style = :expanded
+# line_comments = true
 
-# 3. You can select your preferred output style here (can be overridden via the command line):
-output_style = :expanded
-
-# 4. When you are ready to launch your WP theme comment out (3) and uncomment the line below
-# output_style = :compressed
-
-# To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+# 4. Settings for production
+environment = :production
+output_style = :compressed
+line_comments = false
 
 # don't touch this
 preferred_syntax = :scss
