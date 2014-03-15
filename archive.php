15a633587e7123dce35fe7c1df4ca08d391ceeac
rclations <rc@hallme.com>
2014-03-15 19:18:51 -0400

    Updated archive template to match _s

--- archive.php
+++ archive.php
@@ -1,42 +1,78 @@
-<?php get_header(); ?>
+<?php
+/**
+ * The template for displaying Archive pages.
+ *
+ * Learn more: http://codex.wordpress.org/Template_Hierarchy
+ */
 
+get_header(); ?>
 
-			<?php if (is_category()) { ?>
+		<?php if ( have_posts() ) : ?>
+
+			<header class="page-header">
 				<h1 class="archive-title">
-					<span><?php _e("Posts Categorized:", "scaffoldingtheme"); ?></span> <?php single_cat_title(); ?>
+					<?php
+						if ( is_category() ) :
+							single_cat_title();
+
+						elseif ( is_tag() ) :
+							single_tag_title();
+
+						elseif ( is_author() ) :
+							printf( __( 'Author Archive: %s', 'test' ), '<span class="vcard">' . get_the_author() . '</span>' );
+
+						elseif ( is_day() ) :
+							printf( __( 'Daily Archives: %s', 'test' ), '<span>' . get_the_date() . '</span>' );
+
+						elseif ( is_month() ) :
+							printf( __( 'Monthly Archives: %s', 'test' ), '<span>' . get_the_date( _x( 'F Y', 'monthly archives date format', 'test' ) ) . '</span>' );
+
+						elseif ( is_year() ) :
+							printf( __( 'Yearly Archives: %s', 'test' ), '<span>' . get_the_date( _x( 'Y', 'yearly archives date format', 'test' ) ) . '</span>' );
+
+						elseif ( is_tax( 'post_format', 'post-format-aside' ) ) :
+							_e( 'Asides', 'test' );
+
+						elseif ( is_tax( 'post_format', 'post-format-gallery' ) ) :
+							_e( 'Galleries', 'test');
+
+						elseif ( is_tax( 'post_format', 'post-format-image' ) ) :
+							_e( 'Images', 'test');
+
+						elseif ( is_tax( 'post_format', 'post-format-video' ) ) :
+							_e( 'Videos', 'test' );
+
+						elseif ( is_tax( 'post_format', 'post-format-quote' ) ) :
+							_e( 'Quotes', 'test' );
+
+						elseif ( is_tax( 'post_format', 'post-format-link' ) ) :
+							_e( 'Links', 'test' );
+
+						elseif ( is_tax( 'post_format', 'post-format-status' ) ) :
+							_e( 'Statuses', 'test' );
+
+						elseif ( is_tax( 'post_format', 'post-format-audio' ) ) :
+							_e( 'Audios', 'test' );
+
+						elseif ( is_tax( 'post_format', 'post-format-chat' ) ) :
+							_e( 'Chats', 'test' );
+
+						else :
+							_e( 'Archives', 'test' );
+
+						endif;
+					?>
 				</h1>
+				<?php
+					// Show an optional term description.
+					$term_description = term_description();
+					if ( ! empty( $term_description ) ) :
+						printf( '<div class="taxonomy-description">%s</div>', $term_description );
+					endif;
+				?>
+			</header><!-- .page-header -->
 
-			<?php } elseif (is_tag()) { ?>
-				<h1 class="archive-title">
-					<span><?php _e("Posts Tagged:", "scaffoldingtheme"); ?></span> <?php single_tag_title(); ?>
-				</h1>
-
-			<?php } elseif (is_author()) {
-				global $post;
-				$author_id = $post->post_author;
-			?>
-				<h1 class="archive-title">
-
-					<span><?php _e("Posts By:", "scaffoldingtheme"); ?></span> <?php the_author_meta('display_name', $author_id); ?>
-
-				</h1>
-			<?php } elseif (is_day()) { ?>
-				<h1 class="archive-title">
-					<span><?php _e("Daily Archives:", "scaffoldingtheme"); ?></span> <?php the_time('l, F j, Y'); ?>
-				</h1>
-
-			<?php } elseif (is_month()) { ?>
-				<h1 class="archive-title">
-					<span><?php _e("Monthly Archives:", "scaffoldingtheme"); ?></span> <?php the_time('F Y'); ?>
-				</h1>
-
-			<?php } elseif (is_year()) { ?>
-				<h1 class="archive-title">
-					<span><?php _e("Yearly Archives:", "scaffoldingtheme"); ?></span> <?php the_time('Y'); ?>
-				</h1>
-			<?php } ?>
-
-			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
+			<?php while (have_posts()) : the_post(); ?>
 
 					<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article">
 
