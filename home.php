7a8a66742187baf31910fa0edd014e9efad1e800
Jess Lavoie <jessicamlavoie.com>
2014-05-07 09:42:43 -0400

    housekeeping stuff

--- home.php
+++ home.php
@@ -1,7 +1,5 @@
 <?php get_header(); ?>
 
-
-
 			<?php if (have_posts()) : while (have_posts()) : the_post(); ?>
 
 					<article id="post-<?php the_ID(); ?>" <?php post_class('clearfix'); ?> role="article">
