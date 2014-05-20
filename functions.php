88157d6d75603004440792e279322f83694ced31
Tim Howe <timbhowe@gmail.com>
2014-05-20 17:09:24 -0400

    added backgroundsize pollyfill

--- functions.php
+++ functions.php
@@ -185,6 +185,7 @@ register_default_headers( array(
 function scaffolding_custom_headers_callback() {
 	?>	<style type="text/css">#banner {
 			background-image: url(<?php header_image(); ?>);
+			-ms-behavior: url(<?php echo get_template_directory_uri(); ?>/includes/backgroundsize.min.htc);
 		}</style><?php
 }
 
