310281d54c4c5ad4adb8276614c99e0aa0797ba8
Tim Howe <timbhowe@gmail.com>
2014-05-21 09:45:59 -0400

    removed redundant comment tags updated sidebar classes in the footer to prevent overlap

--- footer.php
+++ footer.php
@@ -1,14 +1,7 @@
-<?php
-/**
- * The template for displaying the footer.
- *
- * Contains the closing of the #content div and all content after
- */
-?>
 		</div><?php //END #main ?>
 
 	<?php if ( is_active_sidebar( 'left-sidebar' ) ) : ?>
-		<div id="left-sidebar" class="sidebar col-sm-3 col-md-pull-9 clearfix" role="complementary">
+		<div id="left-sidebar" class="sidebar col-sm-3 col-sm-pull-9 clearfix" role="complementary">
 			<?php dynamic_sidebar( 'left-sidebar' ); ?>
 		</div>
 	<?php endif; ?>
--- header.php
+++ header.php
@@ -1,10 +1,4 @@
-<?php
-/**
- * The template for displaying the header.
- *
- * Contains the opening tag for the page structure
- */
-?><!DOCTYPE html>
+<!DOCTYPE html>
 <!--[if lt IE 7]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
 <!--[if (IE 7)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9 lt-ie8"><![endif]-->
 <!--[if (IE 8)&!(IEMobile)]><html <?php language_attributes(); ?> class="no-js lt-ie9"><![endif]-->
