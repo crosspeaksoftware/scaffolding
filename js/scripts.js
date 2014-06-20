4d8af5bfd1bc2b45dc99b2171d4773e2c629db28
Matt Harrison <matt@hallme.com>
2014-06-20 15:37:15 -0400

    combine scripts into a script.js file

---
+++ js/scripts.js
@@ -0,0 +1,63 @@
+// Set parallax on larger screens
+var isMobile = {
+	Android: function() {
+		return navigator.userAgent.match(/Android/i);
+	},
+	BlackBerry: function() {
+		return navigator.userAgent.match(/BlackBerry/i);
+	},
+	iOS: function() {
+		return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+	},
+	Opera: function() {
+		return navigator.userAgent.match(/Opera Mini/i);
+	},
+	Windows: function() {
+		return navigator.userAgent.match(/IEMobile/i);
+	},
+	any: function() {
+		return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+	}
+};
+$(function() {
+	// skrollr
+	if( !isMobile.any() ){
+		skrollr.init({
+			forceHeight: false,
+			mobileCheck: function() {
+				//hack - forces mobile version to be off
+				return false;
+			}
+		});
+	};
+
+	// Analytics tracking
+
+	// Download Button
+	$("#download-link").click(function() {
+		ga('send', 'event', 'button', 'click', 'Download');
+	});
+	// Customize Button
+	$("#customize button").click(function() {
+		ga('send', 'event', 'button', 'click', 'Customize');
+	});
+	$("#getstarted_btn").click(function() {
+		ga('send', 'event', 'button', 'click', 'Get Started');
+	});
+	// Fork on GitHub Button
+	$("#forkongithub_btn").click(function() {
+		ga('send', 'event', 'button', 'click', 'Get Started');
+	});
+	$("#name").change(function() {
+		ga('send', 'event', 'Form', 'Field', 'Name = ' + $(this).val());
+	});
+	$("#slug").change(function() {
+		ga('send', 'event', 'Form', 'Field', 'Slug = ' + $(this).val());
+	});
+	$("#author").change(function() {
+		ga('send', 'event', 'Form', 'Field', 'Author = ' + $(this).val());
+	});
+	$("#authoruri").change(function() {
+		ga('send', 'event', 'Form', 'Field', 'Author URI = ' + $(this).val());
+	});
+});
