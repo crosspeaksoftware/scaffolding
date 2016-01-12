2edd68c036707b30fd47e7d7a4eca38b79fdd52f
jlavoie <jess@hallme.com>
2016-01-12 09:56:59 -0500

    remove favicon and touch icon meta in favor of using the new site icon feature in wp

--- header.php
+++ header.php
@@ -17,14 +17,6 @@
 <meta name="HandheldFriendly" content="True">
 <meta name="MobileOptimized" content="320">
 <meta name="viewport" content="width=device-width, initial-scale=1"/>
-
-<?php // icons & favicons (for more: http://www.jonathantneal.com/blog/understand-the-favicon/) ?>
-<link rel="apple-touch-icon" href="<?php echo get_template_directory_uri(); ?>/images/apple-icon-touch.png">
-<link rel="icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
-<!--[if IE]><link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico"><![endif]-->
-<meta name="msapplication-TileColor" content="#f01d4f">
-<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/images/win8-tile-icon.png">
-
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 
