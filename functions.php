679b1596e2b8f039220320ae8d2ea4c10fbf35c6
jlavoie <jess@hallme.com>
2016-02-10 14:10:20 -0500

    integrating alerts with woo and gravity forms, and various style fixes

--- functions.php
+++ functions.php
@@ -73,7 +73,6 @@ function scaffolding_scripts_and_styles() {
 	// Main stylesheet
 	wp_enqueue_style( 'scaffolding-stylesheet', get_stylesheet_directory_uri() . '/css/style.css', array(), '', 'all' );
 
-
 	// Font Awesome (icon set) - http://fortawesome.github.io/Font-Awesome/
 	wp_enqueue_style( 'scaffolding-font-awesome', get_stylesheet_directory_uri() . '/libs/font-awesome/css/font-awesome.min.css', array(), '4.5.0' );
 
