751af7435c264ebd4a415144252423276710be22
jlavoie13 <jess@hallme.com>
2017-08-18 16:06:56 -0400

    form edits

--- js/scripts.js
+++ js/scripts.js
@@ -265,30 +265,6 @@ jQuery(document).ready(function($) {
 		});
 	});
 
-	/*
-	// Released under MIT license: http://www.opensource.org/licenses/mit-license.php
-	$('[placeholder]').focus(function() {
-		var input = $(this);
-		if (input.val() == input.attr('placeholder')) {
-			input.val('');
-			input.removeClass('placeholder');
-		}
-	}).blur(function() {
-		var input = $(this);
-		if (input.val() == '' || input.val() == input.attr('placeholder')) {
-			input.addClass('placeholder');
-			input.val(input.attr('placeholder'));
-		}
-	}).blur().parents('form').submit(function() {
-		$(this).find('[placeholder]').each(function() {
-			var input = $(this);
-			if (input.val() == input.attr('placeholder')) {
-				input.val('');
-			}
-		})
-	});
-	*/
-
 }); /* end of as page load scripts */
 
 /*
