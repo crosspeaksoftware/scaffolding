ab17df14990f1c1b246858700316b9632affac0c
jlavoie13 <jess@hallme.com>
2019-02-26 15:41:42 -0500

    move walker nav into separate file

--- functions.php
+++ functions.php
@@ -46,6 +46,7 @@ define( 'SCAFFOLDING_INCLUDE_PATH', dirname( __FILE__ ) . '/includes/' );
 
 // Add any additional files to include here.
 require_once SCAFFOLDING_INCLUDE_PATH . 'base-functions.php';
+require_once SCAFFOLDING_INCLUDE_PATH . 'class-scaffolding-walker-nav-menu.php';
 require_once SCAFFOLDING_INCLUDE_PATH . 'tinymce-settings.php';
 // require_once( SCAFFOLDING_INCLUDE_PATH . 'theme-guide.php' );
 // Gravity Forms Customizations.
@@ -289,13 +290,13 @@ function scaffolding_theme_support() {
  *
  * Add any additional menus here. Register new menu in scaffolding_theme_support() above.
  *
- * @see scaffolding_walker_nav_menu
  * @since Scaffolding 1.0
  */
 
 /**
  * Main navigation menu
  *
+ * @see Scaffolding_Walker_Nav_Menu
  * @return void
  */
 function scaffolding_main_nav() {
@@ -340,129 +341,6 @@ function scaffolding_footer_nav() {
 		)
 	);
 } // end scaffolding_footer_nav()
-
-/**
- * Custom walker to build main navigation menu
- *
- * Adds classes for enhanced styles and support for mobile off-canvas menu.
- *
- * @since Scaffolding 1.0
- */
-class Scaffolding_Walker_Nav_Menu extends Walker_Nav_Menu {
-	/**
-	 * Starts the list before the elements are added.
-	 *
-	 * @see Walker::start_lvl()
-	 *
-	 * @param string   $output Used to append additional content (passed by reference).
-	 * @param int      $depth  Depth of menu item. Used for padding.
-	 * @param stdClass $args   An object of wp_nav_menu() arguments.
-	 */
-	public function start_lvl( &$output, $depth = 0, $args = array() ) {
-		// depth dependent classes.
-		$indent        = ( $depth > 0 ? str_repeat( "\t", $depth ) : '' ); // code indent.
-		$display_depth = ( $depth + 1 ); // because it counts the first submenu as 0.
-		$classes       = array(
-			'sub-menu',
-			( $display_depth % 2 ? 'menu-odd' : 'menu-even' ),
-			'menu-depth-' . $display_depth,
-		);
-		$class_names   = implode( ' ', $classes );
-
-		// build html.
-		$output .= "\n" . $indent . '<ul class="' . $class_names . '"><li><button class="menu-back-button" type="button"><i class="fa fa-chevron-left"></i> Back</button></li>' . "\n";
-	}
-
-	/**
-	 * Starts the element output.
-	 *
-	 * @see Walker::start_el()
-	 *
-	 * @param string   $output Used to append additional content (passed by reference).
-	 * @param WP_Post  $item   Menu item data object.
-	 * @param int      $depth  Depth of menu item. Used for padding.
-	 * @param stdClass $args   An object of wp_nav_menu() arguments.
-	 * @param int      $id     Current item ID.
-	 */
-	public function start_el( &$output, $item, $depth = 0, $args = array(), $id = 0 ) {
-		global $wp_query;
-		$indent      = ( $depth ) ? str_repeat( "\t", $depth ) : '';
-		$class_names = $value = '';
-
-		// set li classes.
-		$classes = empty( $item->classes ) ? array() : (array) $item->classes;
-		if ( ! $args->has_children ) {
-			$classes[] = 'menu-item-no-children';
-		}
-
-		// combine the class array into a string.
-		$class_names = join( ' ', apply_filters( 'nav_menu_css_class', array_filter( $classes ), $item, $args ) );
-		$class_names = ' class="' . esc_attr( $class_names ) . '"';
-
-		// set li id.
-		$id = apply_filters( 'nav_menu_item_id', 'menu-item-' . $item->ID, $item, $args );
-		$id = strlen( $id ) ? ' id="' . esc_attr( $id ) . '"' : '';
-
-		// set outer li and its attributes.
-		$output .= $indent . '<li' . $id . $value . $class_names . '>';
-
-		// set link attributes.
-		$attributes  = ! empty( $item->attr_title ) ? ' title="' . esc_attr( $item->attr_title ) . '"' : ' title="' . esc_attr( strip_tags( $item->title ) ) . '"';
-		$attributes .= ! empty( $item->target ) ? ' target="' . esc_attr( $item->target ) . '"' : '';
-		$attributes .= ! empty( $item->xfn ) ? ' rel="' . esc_attr( $item->xfn ) . '"' : '';
-		$attributes .= ! empty( $item->url ) ? ' href="' . esc_attr( $item->url ) . '"' : '';
-
-		// Add menu button links to items with children.
-		if ( $args->has_children ) {
-			$menu_pull_link = '<button class="menu-button" type="button"><i class="fa fa-chevron-right"></i></button>';
-		} else {
-			$menu_pull_link = '';
-		}
-
-		$item_output  = $args->before;
-		$item_output .= '<a' . $attributes . '>';
-		$item_output .= $args->link_before . apply_filters( 'the_title', $item->title, $item->ID ) . $args->link_after;
-		$item_output .= '</a>';
-		$item_output .= $menu_pull_link . $args->after;
-
-		$output .= apply_filters( 'walker_nav_menu_start_el', $item_output, $item, $depth, $args );
-	}
-
-	/**
-	 * Ends the element output, if needed.
-	 *
-	 * @see Walker::end_el()
-	 *
-	 * @param string   $output Used to append additional content (passed by reference).
-	 * @param WP_Post  $item   Page data object. Not used.
-	 * @param int      $depth  Depth of page. Not Used.
-	 * @param stdClass $args   An object of wp_nav_menu() arguments.
-	 */
-	public function end_el( &$output, $item, $depth = 0, $args = array() ) {
-		$output .= "</li>\n";
-	}
-
-	/**
-	 * Add additional arguments to use for building the markup.
-	 *
-	 * @param WP_Post  $element           Menu item data object.
-	 * @param WP_Post  $children_elements Menu item data object (passed by reference).
-	 * @param int      $max_depth         Max depth of page. Not used.
-	 * @param int      $depth             Depth of page. Not used.
-	 * @param stdClass $args              An object of wp_nav_menu() arguments.
-	 * @param string   $output            Used to append additional content (passed by reference).
-	 */
-	public function display_element( $element, &$children_elements, $max_depth, $depth = 0, $args, &$output ) {
-
-		// Set custom arg to tell if item has children.
-		$id_field = $this->db_fields['id'];
-		if ( is_object( $args[0] ) ) {
-			$args[0]->has_children = ! empty( $children_elements[ $element->$id_field ] );
-		}
-
-		return parent::display_element( $element, $children_elements, $max_depth, $depth, $args, $output );
-	}
-} // end Scaffolding_Walker_Nav_Menu()
 
 
 /************************************
