fa6b2a9489caba97b96275511e5019aebd24cc9d
jlavoie13 <jess@hallme.com>
2019-08-13 11:23:54 -0400

    search edits

--- searchform.php
+++ searchform.php
@@ -14,7 +14,7 @@ if ( empty( $sc_search_form_index ) ) {
 $sc_index = $sc_search_form_index++;
 
 ?>
-<form method="get" class="sc-searchform clearfix" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
+<form method="get" class="sc-searchform" action="<?php echo esc_url( home_url( '/' ) ); ?>" role="search">
 	<label class="screen-reader-text" for="sc-searchform-field-<?php echo isset( $sc_index ) ? absint( $sc_index ) : 0; ?>"><?php esc_html_e( 'Search for:', 'scaffolding' ); ?></label>
 	<input type="search" class="sc-searchform-field" name="s" id="sc-searchform-field-<?php echo isset( $sc_index ) ? absint( $sc_index ) : 0; ?>" value="<?php echo esc_attr( get_search_query() ); ?>" placeholder="<?php esc_attr_e( 'Search the Site&hellip;', 'scaffolding' ); ?>">
 	<button type="submit" value="<?php esc_attr_e( 'Search', 'scaffolding' ); ?>"><?php esc_attr_e( 'Go', 'scaffolding' ); ?></button>
