575113a45a3ca75f14a38f894bde80710b1aa932
Tim Howe <tim@hallme.com>
2022-06-15 13:13:36 -0400

    Removed tinymce settings include

--- functions.php
+++ functions.php
@@ -36,16 +36,19 @@ define( 'SCAFFOLDING_INCLUDE_PATH', dirname( __FILE__ ) . '/includes/' );
 // Add any additional files to include here.
 require_once SCAFFOLDING_INCLUDE_PATH . 'base-functions.php';
 require_once SCAFFOLDING_INCLUDE_PATH . 'class-scaffolding-walker-nav-menu.php';
-require_once SCAFFOLDING_INCLUDE_PATH . 'tinymce-settings.php';
+
 // require_once( SCAFFOLDING_INCLUDE_PATH . 'theme-guide.php' );
+
 // WooCommerce Customizations.
 if ( function_exists( 'is_woocommerce' ) ) {
 	require_once SCAFFOLDING_INCLUDE_PATH . 'woocommerce-customizations.php';
 }
+
 // Gravity Forms Customizations.
 if ( class_exists( 'GFForms' ) ) {
 	require_once SCAFFOLDING_INCLUDE_PATH . 'gf-customizations.php';
 }
+
 // commonWP Support https://wordpress.org/plugins/commonwp/ for details.
 require_once SCAFFOLDING_INCLUDE_PATH . 'commonwp.php';
 
