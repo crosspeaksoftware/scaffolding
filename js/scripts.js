c6de3f220bf5a0ed49abb4b99b8b33e575213ebd
Tim Howe <tim@hallme.com>
2022-12-15 09:26:13 -0500

    Removed function for calculating scrollbar width Cleaned up Comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,5 @@
 /**
- * Site Name:
- * Author:
- *
- * Name: Scaffolding Scripts
+ * Scripts
  *
  * @package scaffolding
  *
@@ -11,35 +8,6 @@
  * this file will be called automatically in the footer so as not to
  * slow the page load.
  */
-
-// Calculate the width of the scroll bar so css media queries and js widow.width match.
-function getScrollBarWidth () {
-	var inner          = document.createElement( 'p' );
-	inner.style.width  = "100%";
-	inner.style.height = "200px";
-
-	var outer              = document.createElement( 'div' );
-	outer.style.position   = "absolute";
-	outer.style.top        = "0px";
-	outer.style.left       = "0px";
-	outer.style.visibility = "hidden";
-	outer.style.width      = "200px";
-	outer.style.height     = "150px";
-	outer.style.overflow   = "hidden";
-	outer.appendChild( inner );
-
-	document.body.appendChild( outer );
-	var w1               = inner.offsetWidth;
-	outer.style.overflow = 'scroll';
-	var w2               = inner.offsetWidth;
-	if (w1 == w2) {
-		w2 = outer.clientWidth;
-	}
-
-	document.body.removeChild( outer );
-
-	return (w1 - w2);
-};
 
 // As the page loads, call these scripts.
 jQuery( document ).ready(
